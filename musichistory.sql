ed9e7c6e169f3269285d4fd2130a32f600ff1fa0
MandasK <king.amanda.r@gmail.com>
2020-08-31 16:04:05 -0500

    last few exercises

---
+++ musichistory.sql
@@ -0,0 +1,41 @@
+﻿Select Count(*) as 'Labels',
+a.ArtistName
+from Album
+left Join Artist a on Album.ArtistId = a.id
+Group By ArtistName
+Having Count(distinct Label) > 1 ;
+
+Select *,
+a.ArtistName
+from Album
+left join Artist a on Album.ArtistId = a.id;
+
+Select *
+from Artist
+Order by ArtistName;
+
+Select 
+SongLength,
+title
+
+from Song
+where 
+	SongLength = (
+	select
+		Max(SongLength)
+		from
+			Song
+			);
+select AlbumLength,
+	title
+	from Album
+	where AlbumLength = (
+		select MAX(AlbumLength)
+		from Album
+	)
+
+
+
+	
+
+	
